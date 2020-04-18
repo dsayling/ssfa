862fe1e5f9b648fc5432aba2e5977df482b5fb3d
Drew Ayling <dsayling@gmail.com>
2020-04-18 17:51:45 -0600

    Adding tests
    Adding some sanity tests to ensure the env is setup without selenium
    Adding a selenium based test
    Refactor app.py to support easier selenium testing
    Add new.html to support new render that allows easier check
    Update README with highlevel test instructions
    
    https://chromedriver.storage.googleapis.com/index.html?path=81.0.4044.69/#

--- app.py
+++ app.py
@@ -2,15 +2,31 @@ import flask
 
 app = flask.Flask(__name__)
 
+# info for index.html, loaded on GET
+BUTTON_NAME = 'button'
+ELEM_NAME = 'mtext'
+WELCOME_TEXT = 'Hello! Click the button for new text'
+BUTTON_TEXT = 'Click me!'
+
+# info for new.html, loaded on POST
+NEW_TEXT = 'New text. Be amazed!'
+NEW_ELEM_NAME = 'new'
+
 @app.route('/')
 def home():
     """Render a template with the first text and button."""
-    return flask.render_template('index.html')
+    return flask.render_template('index.html',
+                                 mtext=WELCOME_TEXT,
+                                 mtext_name=ELEM_NAME,
+                                 bname=BUTTON_NAME,
+                                 btext=BUTTON_TEXT)
 
 @app.route('/', methods=['POST'])
 def home_post():
     """When the submit is done, we should load this text."""
-    return 'New text. Be amazed!'
+    return flask.render_template('new.html',
+                                 ntext=NEW_TEXT,
+                                 nname=NEW_ELEM_NAME)
 
 if __name__ == "__main__":
     # leave it on port 5000 for permission sake
---
+++ tests/test_app.py
@@ -0,0 +1,37 @@
+"""Acceptance tests.
+
+Was going to use pytest, but why add another dependency.
+"""
+
+import unittest
+from selenium import webdriver
+import selenium
+
+import app
+
+
+class AcceptanceTest(unittest.TestCase):
+
+    @unittest.expectedFailure
+    def test_false(self):
+        assert False
+    
+    def test_true(self):
+        assert True
+    
+    def _generic_test(self, wdriver):
+        with wdriver() as driver:
+            driver.get("http://localhost:5000")
+            elem1 = driver.find_element_by_name(app.ELEM_NAME)
+            assert elem1.text == app.WELCOME_TEXT
+            elem2 = driver.find_element_by_name(app.BUTTON_NAME)
+            elem2.click()
+            driver.find_element_by_name(app.NEW_ELEM_NAME)
+
+    def test_selenium_chrome(self):
+        self._generic_test(webdriver.Chrome)
+
+    # mark this as a failure since I'm just going to get chrome going
+    @unittest.expectedFailure
+    def test_selenium_firefox(self):
+        self._generic_test(webdriver.Firefox)
